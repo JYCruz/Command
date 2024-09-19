df1c25b790a2d305ffb04043704f304558678ad1
JYCruz <josh.cruz@neu.edu.ph>
2024-09-20 00:23:04 +0800

    Create Thermostat.java

---
+++ Thermostat.java
@@ -0,0 +1,19 @@
+package Command;
+
+public class Thermostat implements Device{
+    public String switchOn(){
+        return "Thermostat on";
+    }
+
+    public String switchOff(){
+        return "Thermostat off";
+    }
+
+    public String increase(){
+        return "Increasing temperature";
+    }
+    
+    public String decrease(){
+        return "Decreasing temperature";
+    }
+}
