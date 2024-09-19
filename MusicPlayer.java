5c7c9049d56b47eb1eb9c968a5a8da6c18f5a8e9
JYCruz <josh.cruz@neu.edu.ph>
2024-09-20 00:16:50 +0800

    Create MusicPlayer.java

---
+++ MusicPlayer.java
@@ -0,0 +1,21 @@
+package Command;
+
+public class MusicPlayer implements Device{
+    
+    public String switchOn(){
+        return "Music player on. Playing playlist";
+    }
+
+    public String switchOff(){
+        return "Music player off. Stopping music";
+    }
+
+    public String increase(){
+        return "Volume increased";
+    }
+
+    public String decrease(){
+        return "Volume decreased";
+    }
+    
+}
